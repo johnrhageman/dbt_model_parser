cad295b756d6d6cd5c42f9db3391724dc0e033c3
John Hageman <johnhageman@John-Hageman.local>
2020-02-19 10:54:03 -0600

    Initial commit

---
+++ dbt-model-parser.py
@@ -0,0 +1,35 @@
+import sqlparse
+import argparse
+import yaml
+
+parser = argparse.ArgumentParser(description='Generate column spec from copiled dbt model')
+parser.add_argument('--model', dest='filename', help='The path to the compiled dbt model SQL, typically in project/target/compiled/project_name folder')
+
+args = parser.parse_args()
+
+model_name = str(args.filename.split('/')[-1]).replace('.sql', '')
+
+with open(args.filename, 'r') as myfile:
+  sql_query = myfile.read()
+
+col_list = []
+s = sqlparse.parse(sql_query)
+token_list = s[0].tokens
+for token in token_list:
+    if isinstance(token, sqlparse.sql.IdentifierList):
+        for identifier in token.get_identifiers():
+           
+            if(' AS ' in str(identifier)):
+                split_id = str(identifier).split(' AS ')
+                
+                col_list.append(split_id[-1])
+            elif('.' in str(identifier)):
+                split_id = str(identifier).split('.')
+                
+                col_list.append(split_id[-1])
+
+
+x = [{"name": x, "description": "description of {}".format(x), "tests": ["not_null"]} for x in col_list]
+yaml_dict = {"models": {"name": model_name, "description": "description of {}".format(model_name), "columns": x}}
+
+print(yaml.dump(yaml_dict, default_flow_style=False, sort_keys=False))
